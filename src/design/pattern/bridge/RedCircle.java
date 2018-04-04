5680708c23e6e36ab89ff545fc5937279df69acf
baiyooooooo <baiyooooooo@users.noreply.github.com>
2018-04-04 23:25:35 +0800

    Update RedCircle.java

--- src/design/pattern/bridge/RedCircle.java
+++ src/design/pattern/bridge/RedCircle.java
@@ -2,10 +2,10 @@ package design.pattern.bridge;
 
 public class RedCircle implements IDraw{
 
-// 	@Override
-// 	public void drawCircle(int radius, int x, int y) {
-// 		// TODO Auto-generated method stub
-// 		System.out.println("Draw circle[ color: red, radius: " + radius + ", x: " + x + ", y: " + y + "]");
-// 	}
+	@Override
+	public void drawCircle(int radius, int x, int y) {
+		// TODO Auto-generated method stub
+		System.out.println("Draw circle[ color: red, radius: " + radius + ", x: " + x + ", y: " + y + "]");
+	}
 
 }
